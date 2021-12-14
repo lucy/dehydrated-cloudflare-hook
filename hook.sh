0168c20e754c0b99d74ededbdb11b9ac27b70690
lucy <lucy@luz.lu>
2021-12-14 10:44:38 +0100

    suppress output on success and print nicer errors

--- hook.sh
+++ hook.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
-# TODO: prettier output instead of raw json i guess
-set -e -o pipefail -x
+set -e -o pipefail
 
 # req <method> <path> <curl arg ...>
 req() {
-	curl -s -X "$1" "https://api.cloudflare.com/client/v4/$2" \
-		-H "Authorization: Bearer $CF_API_KEY" "${@:3}"
+	local out
+	out="$(curl -s -X "$1" "https://api.cloudflare.com/client/v4/$2" \
+		-H "Authorization: Bearer $CF_API_KEY" "${@:3}")"
+	if [[ "$(printf '%s' "$out" | jq -r '.success')" != true ]]; then
+		printf 'error from cloudflare api:\n%s\n' "$out" 1>&2
+		exit 1
+	fi
+	printf '%s' "$out"
 }
 
 # zid <domain>
@@ -16,18 +21,18 @@ rid() { req GET "zones/$1/dns_records?type=TXT&name=$2&content=$3" | jq -r '.res
 
 case "$1" in
 deploy_challenge)
-	DOMAIN="$2" TOKEN_FILENAME="$3" TOKEN_VALUE="$4"
+	DOMAIN="$2" TOKEN="$4"
 	ZID="$(zid "$DOMAIN")"
-	req POST "zones/$ZID/dns_records" --data \
-		"{\"type\":\"TXT\",\"name\":\"_acme-challenge.$DOMAIN\",\"content\":\"$TOKEN_VALUE\",\"ttl\":60}"
+	data="{\"type\":\"TXT\",\"name\":\"_acme-challenge.$DOMAIN\",\"content\":\"$TOKEN\",\"ttl\":60}"
+	req POST "zones/$ZID/dns_records" --data "$data" >/dev/null
 	printf 'waiting for %d seconds\n' 10 1>&2
 	sleep 10
 	;;
 clean_challenge)
-	DOMAIN="$2" TOKEN_FILENAME="$3" TOKEN_VALUE="$4"
+	DOMAIN="$2" TOKEN="$4"
 	ZID="$(zid "$DOMAIN")"
 	RID="$(rid "$ZID" "_acme-challenge.$DOMAIN" "$TOKEN_VALUE")"
-	req DELETE "zones/$ZID/dns_records/$RID"
+	req DELETE "zones/$ZID/dns_records/$RID" >/dev/null
 	;;
 invalid_challenge)
 	DOMAIN="$2" RESPONSE="$3"
@@ -36,8 +41,8 @@ invalid_challenge)
 # deploy_cert)
 # 	DOMAIN="$2" KEYFILE="$3" CERTFILE="$4" FULLCHAINFILE="$5" CHAINFILE="$6" TIMESTAMP="$7"
 # 	for f in "$KEYFILE" "$CERTFILE" "$FULLCHAINFILE" "$CHAINFILE"; do
-# 		chown acme:tlscert "$f"
 # 		chmod 640 "$f"
+# 		chown acme:tlscert "$f"
 # 	done
 # 	systemctl restart nginx.service
 # 	;;
